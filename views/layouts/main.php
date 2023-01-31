4ee0af99b2dfcda22a5f924a01d2259e033782d8
Oybek Nematov <on.1349@list.ru>
2023-01-31 23:07:50 +0500

    123

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -36,6 +36,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
         'brandUrl' => Yii::$app->homeUrl,
         'options' => ['class' => 'navbar-expand-md navbar-dark bg-dark fixed-top']
     ]);
+    
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav'],
         'items' => [
